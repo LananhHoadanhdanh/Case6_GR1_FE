84632e31610b369b746d1f0d25eb47d9023e200e
ak88vp <tiendung2020japan@gmail.com>
2022-02-09 14:55:27 +0700

    UI Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./component/login/login.component";
+import {UpdateUserComponent} from "./module/user/update-user/update-user.component";
 
 const routes: Routes = [
   {path: '',
-  component: LoginComponent}
+  component: LoginComponent},
+  {
+  path:'user',
+    loadChildren :() => import('./module/user/user.module').then(module => module.UserModule)
+  }
 ];
 
 @NgModule({
--- src/app/module/user/user-routing.module.ts
+++ src/app/module/user/user-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {UpdateUserComponent} from "./update-user/update-user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomepageComponent
+  },{
+    path:'update',
+    component:UpdateUserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
