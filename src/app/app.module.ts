906cd5d276b785df2c1db02d62c84e64e954f254
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-02-09 11:15:21 +0700

    chia module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListUserComponent } from './component/list-user/list-user.component';
+import { ListUserComponent } from './module/user/list-user/list-user.component';
+import { LoginComponent } from './component/login/login.component';
+import { UpdateUserComponent } from './module/user/update-user/update-user.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListUserComponent
+    ListUserComponent,
+    LoginComponent,
+    UpdateUserComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
