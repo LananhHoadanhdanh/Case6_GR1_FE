e680b66e6b207ca283467b61e7c2bf5b80c6bab5
tekdoken <tekdokenzx@gmail.com>
2022-02-09 21:50:05 +0700

    component SERVICE

--- src/app/module/user/user.module.ts
+++ src/app/module/user/user.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import {UpdateUserComponent} from "./update-user/update-user.component";
+import { UpdateServiceComponent } from './update-service/update-service.component';
 
 
 @NgModule({
   declarations: [
     HomepageComponent,
-    UpdateUserComponent
+    UpdateUserComponent,
+    UpdateServiceComponent
   ],
   imports: [
     CommonModule,
