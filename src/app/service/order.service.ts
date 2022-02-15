7ee90dd89107af3569e1ecfd5001dc54552dd12e
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-02-15 10:10:55 +0700

    status service

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Image} from "../model/image";
+import {environment} from "../../environments/environment";
+import {Order} from "../model/order";
+const API_URL = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAllOrderByRenter(id: any) :Observable<Order[]>{
+    return this.http.get<Order[]>(API_URL+`/renter/${id}/orders`)
+  }
+
+  findAllOrderByProvider(id: any) :Observable<Order[]>{
+    return this.http.get<Order[]>(API_URL+`/provider/${id}/orders`)
+  }
 }
