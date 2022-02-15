579dc324adaa8b04bbd715331e62eec843f41ba6
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-02-15 15:08:50 +0700

    đơn

--- src/app/module/admin/all-order-list/all-order-list.component.ts
+++ src/app/module/admin/all-order-list/all-order-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from "../../../model/order";
+import {OrderService} from "../../../service/order.service";
+import swal from "sweetalert";
 
 @Component({
   selector: 'app-all-order-list',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-order-list.component.css']
 })
 export class AllOrderListComponent implements OnInit {
+  orders: Order[] = [];
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
+    this.loadAll()
   }
 
+  loadAll() {
+    this.orderService.getAll().subscribe(res => {
+      this.orders = res
+      console.log(res)
+    })
+  }
+
+  deleteOrder(id: any) {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this imaginary file!",
+      icon: "warning",
+      buttons: ['Cancel', 'Delete'],
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          this.orderService.deleteOrder(id).subscribe(() => {
+            swal("Poof! Your imaginary file has been deleted!", {
+              icon: "success",
+            });
+            this.loadAll()
+          })
+        } else {
+          // swal("Your imaginary file is safe!");
+        }
+      });
+  }
 }
